1128f1e3ba98977771eb5e614da054212ca576ed
Athar-Husain <mhatharhusain@gmail.com>
2026-03-27 08:57:14 +0530

    hhh

--- src/app copy 3.js
+++ src/app copy 3.js
@@ -3,24 +3,19 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import avatarRoutes from "./routes/avatar.routes.js";
-import fs from "fs";
 
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-// create upload folders if missing
-["src/public/uploads", "src/public/generated"].forEach((dir) => {
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-});
-
-// ✅ CORS
 app.use(
   cors({
-    origin: FRONTEND_URL || "*",
+    origin: FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
+
+
+app.options("*", cors());
 
 app.use(helmet());
 app.use(express.json());
@@ -32,7 +27,8 @@ app.use("/generated", express.static("src/public/generated"));
 // routes
 app.use("/api/avatar", avatarRoutes);
 
-// health check
-app.get("/", (req, res) => res.send("AI Photobooth Running 🚀"));
+app.get("/", (req, res) => {
+  res.send("AI Photobooth Running 🚀");
+});
 
 export default app;
