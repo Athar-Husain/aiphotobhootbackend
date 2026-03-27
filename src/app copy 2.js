9531a5f6d323d81ccebcd40e54d50d400e0190ac
Athar-Husain <mhatharhusain@gmail.com>
2026-03-27 08:46:50 +0530

    cors added in app

--- src/app copy 2.js
+++ src/app copy 2.js
@@ -1,22 +1,25 @@
-// src/app.js
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import avatarRoutes from "./routes/avatar.routes.js";
 
 const app = express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+// ✅ CORS middleware
 app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
   }),
 );
+
+// ✅ ADD IT HERE (right after cors)
+app.options("*", cors());
+
 app.use(helmet());
 app.use(express.json());
-
-// static folders
-app.use("/uploads", express.static("src/public/uploads"));
-app.use("/generated", express.static("src/public/generated"));
 
 // routes
 app.use("/api/avatar", avatarRoutes);
